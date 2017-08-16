439e371fed2badc64f57464b4ca6605370272d2d
Maxwell Haley <maxwell.haley@openmailbox.org>
2017-08-15 23:06:20 -0400

    Rewrite some of setup.sh to be more robust.
    Add vars that link to book the path to a config file, and the path OF
    the config file itself. Removes having to specify these paths multiple
    times.
    
    `remove_files` now checks first to see if the file exists before running
    `rm`. `workstation_files` now checks to see if the path exists, and runs
    `mkdir` if the path does not exists (assumes `$HOME/.config` exists).

--- setup.sh
+++ setup.sh
@@ -6,44 +6,102 @@
 # This file is license under UNLICENSE, see UNLICENSE for more details or vist
 # http://unlicense.org
 
-dotpath="$HOME/git/dotfiles/"
-configpath="$HOME/.config/"
+dotpath="$HOME/git/dotfiles"
+configpath="$HOME/.config"
+vimpath="$HOME/.vim"
+i3path="$configpath/i3"
+termitepath="$configpath/termite"
+comptonpath="$configpath/compton"
+dunstpath="$configpath/dunst"
+polybarpath="$configpath/polybar"
+
+vimfile="$vimpath/vimrc"
+i3file="$i3path/config"
+termitefile="$termitepath/config"
+comptonfile="$comptonpath/compton.conf"
+dunstfile="$dunstpath/dunstrc"
+polybarfile="$polybarpath/config"
 
 remove_files() {
-    rm ~/.bashrc
-    rm ~/.vimrc
-    rm "$configpath"i3/config
-    rm "$configpath"termite/config
-    rm "$configpath"compton/compton.conf
-    rm "$configpath"dunst/dunstrc
-    rm "$configpath"polybar/config
+    # Removes config files if the file already exists.
+    if [ -f "$HOME/.bashrc" ]; then
+        rm ~/.bashrc
+    fi
+    if [ -f "$HOME/.vimrc" ]; then
+        rm ~/.vimrc #Legacy Remove
+    fi
+    if [ -f "$vimfile" ]; then
+        rm "$vimfile"
+    fi
+    if [ -f "$i3file" ]; then
+        rm "$i3file"
+    fi
+    if [ -f "$termitefile" ]; then
+        rm "$termitefile"
+    fi
+    if [ -f "$comptonfile" ]; then
+        rm "$comptonfile"
+    fi
+    if [ -f "$dunstfile" ]; then
+        rm "$dunstfile"
+    fi
+    if [ -f "$polybarfile" ]; then
+        rm "$polybarfile"
+    fi
 }
 
 common_files() {
-    ln -s "$dotpath"bashrc ~/.bashrc
+    # Creates symlinks for configs destined for any machine.
+
+    ln -s "$dotpath"/bashrc ~/.bashrc
 }
 
 workstation_files() {
-    ln -s "$dotpath"vimrc ~/.vimrc
-    ln -s "$dotpath"i3-config "$configpath"i3/config
-    ln -s "$dotpath"termite-config "$configpath"termite/config
-    ln -s "$dotpath"compton-config "$configpath"compton/compton.conf
-    ln -s "$dotpath"dunstrc "$configpath"dunst/dunstrc
-    ln -s "$dotpath"polybar-config "$configpath"polybar/config
+    # Creates symlinks for all configs destined for a workstation.
+    # Checks if config directory exists, creates it if it doesn't, then creates
+    # the symlink.
+
+    if [ ! -d "$vimpath" ]; then
+        mkdir "$vimpath"
+    fi
+    ln -s "$dotpath"/vimrc "$vimfile"
+    if [ ! -d "$i3path" ]; then
+        mkdir "$i3path"
+    fi
+    ln -s "$dotpath"/i3-config "$i3file"
+    if [ ! -d "$termitepath" ]; then
+        mkdir "$termitepath"
+    fi
+    ln -s "$dotpath"/termite-config "$termitefile"
+    if [ ! -d "$comptonpath" ]; then
+        mkdir "$comptonpath"
+    fi
+    ln -s "$dotpath"/compton-config "$comptonfile"
+    if [ ! -d "$dunstpath" ]; then
+        mkdir "$dunstpath"
+    fi
+    ln -s "$dotpath"/dunstrc "$dunstfile"
+    if [ ! -d "$polybarpath" ]; then
+        mkdir "$polybarpath"
+    fi
+    ln -s "$dotpath"/polybar-config "$polybarfile"
 }
 
 server_files() {
+    # Creates symlinks for all configs destined for a server.
+
     echo "No server files availble.";
 }
 
 remove_files; # Always refresh the links.
-common_files;
 
 case $1 in
     "workstation")
+        common_files;
         workstation_files;
         ;;
     "server")
+        common_files;
         server_files;
         ;;
     "remove")
