cb45e28514c0ef18a19a2b2cf83b2f1bf301c717
Maxwell R. Haley <maxwell.r.haley@gmail.com>
2018-09-13 12:07:01 -0400

    Change magit bindings to call proper functions

--- emacs/.emacs.d/config/my-magit.el
+++ emacs/.emacs.d/config/my-magit.el
@@ -9,13 +9,13 @@
     (evil-set-initial-state 'magit-diff-mode 'normal)
     (evil-set-initial-state 'magit-log-mode 'normal)
     (evil-define-key 'normal magit-mode-map
-        "j" 'magit-goto-next-section
-        "k" 'magit-goto-previous-section)
+        "j" 'magit-section-forward
+        "k" 'magit-section-backward)
     (evil-define-key 'normal magit-log-mode-map
-        "j" 'magit-goto-next-section
-        "k" 'magit-goto-previous-section)
+        "j" 'magit-section-forward
+        "k" 'magit-section-backward)
     (evil-define-key 'normal magit-diff-mode-map
-        "j" 'magit-goto-next-section
-        "k" 'magit-goto-previous-section)))
+        "j" 'magit-section-forward
+        "k" 'magit-section-backward)))
 
 (provide 'my-magit)
