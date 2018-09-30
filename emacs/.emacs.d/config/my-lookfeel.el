146ca46d4a8914a3755f85f7939b782ddadcfb41
Maxwell R. Haley <maxwell.r.haley@gmail.com>
2018-09-30 15:17:22 -0400

    Set font to Hack-10 and updated comments

--- emacs/.emacs.d/config/my-lookfeel.el
+++ emacs/.emacs.d/config/my-lookfeel.el
@@ -1,12 +1,12 @@
-;; linum-relative: Vim-like relative line numbers
-;; https://github.com/coldnew/linum-relative
+;;; linum-relative: Vim-like relative line numbers
+;;; https://github.com/coldnew/linum-relative
 (use-package linum-relative
   :ensure linum-relative
   :init
   (linum-relative-global-mode 1))
 
-;; atom-one-dark-theme: An Emacs port of the Atom One Dark theme from Atom.io.
-;; https://github.com/jonathanchu/atom-one-dark-theme
+;;; atom-one-dark-theme: An Emacs port of the Atom One Dark theme from Atom.io.
+;;; https://github.com/jonathanchu/atom-one-dark-theme
 (use-package atom-one-dark-theme
   :ensure atom-one-dark-theme
   :config
@@ -27,5 +27,8 @@
 
 ;; Disable the toolbar
 (tool-bar-mode -1)
+
+;; Set font
+(add-to-list 'default-frame-alist '(font . "Hack-10"))
 
 (provide 'my-lookfeel)
