d87e37e547b8c5b2e780e035a0fa84fa7c0e31e3
Llewxamris <maxwell.r.haley@gmail.com>
2019-03-21 19:54:45 -0400

    vim-plug installation

--- setup.sh
+++ setup.sh
@@ -1,26 +1,38 @@
 #!/bin/sh
 
 # Friendly Interactive SHell {{{
-sudo pacman -Sy fish
+
+sudo pacman -Sy fish python pkgfile
 echo '/usr/bin/fish' | sudo tee -a /etc/shells
-chsh -s /usr/bin/fish
 mkdir -p ~/.config/fish/
 ln -s ~/git/dotfiles/fish.config ~/.config/fish/config.fish
+
+# Set fish as the default shell
+chsh -s /usr/bin/fish
+
 # }}}
 # Neovim {{{
-sudo pacman -Sy neovim
+
+sudo pacman -Sy neovim python-neovim xsel
 update-alternatives --set /usr/bin/nvim
 mkdir -p ~/.config/nvim/
 mkdir -p ~/.local/share/nvim/plugged/
 mkdir -p ~/.local/share/nvim/undo/
 ln -s ~/git/dotfiles/nvim.config ~/.config/nvim/init.vim
+
+# vim-plug setup
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 nvim +PlugInstall +quitall
 nvim +UpdateRemotePlugins +quitall
+
 #}}}
 # tmux {{{
+
 sudo pacman -Sy tmux
 mkdir -p ~/.config/tmux/
 ln -s ~/git/dotfiles/tmux.config ~/.config/tmux/tmux.config
+
 # }}}
 
 # vim:foldmethod=marker:foldlevel=0
