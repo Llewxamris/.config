4f3c8d07ab909679e6d3a79b90f4147653a76c36
Llewxamris <maxwell.r.haley@gmail.com>
2020-04-19 22:20:06 -0400

    Update dotfiles to use new Makefile for setup
    Instead of using the overly complex `setup.sh` script, I am switching over to
    using a portable Makefile. This Makefile still requires a supporting script, but
    it is far simpler than what I had before. I went with the Makefile for two
    reasons:
    
    1. Support for depenancy management if needed
    2. Integrates nicely with Emacs

---
+++ Makefile
@@ -0,0 +1,42 @@
+.POSIX:
+.SUFFIXES:
+PREFIX = /home/max
+DISTDIR = ./dist
+
+EMACS = emacs
+EMACSFLAGS = --batch --load=org --quick
+
+COPY = cp
+LINKFLAGS = --symbolic --force --relative
+
+RM = rm
+RMFLAGS = --recursive --force
+
+SHELL=/bin/sh
+
+TMUXDEST = .config/tmux/config.tmux
+BASHDEST = .bashrc
+BASHPROFDEST = .bash_profile
+PROFILEDEST = .profile
+
+.PHONY: all install alacritty bash tmux clean uninstall
+
+all: alacritty bash tmux
+install:
+		$(SHELL) -c "./install.sh"
+alacritty:
+		$(EMACS) $(EMACSFLAGS) --eval '(org-babel-tangle-file "alacritty.org")'
+bash:
+		$(EMACS) $(EMACSFLAGS) --eval '(org-babel-tangle-file "bash.org")'
+tmux:
+		$(EMACS) $(EMACSFLAGS) --eval '(org-babel-tangle-file "tmux.org")'
+vim:
+		$(EMACS) $(EMACSFLAGS) --eval '(org-babel-tangle-file "vim.org")'
+clean:
+		$(RM) $(RMFLAGS) $(DISTDIR)
+uninstall:
+		$(RM) $(RMFLAGS) $(PREFIX)/$(FISHDEST)
+		$(RM) $(RMFLAGS) $(PREFIX)/$(TMUXDEST)
+		$(RM) $(RMFLAGS) $(PREFIX)/$(BASHDEST)
+		$(RM) $(RMFLAGS) $(PREFIX)/$(BASHPROFDEST)
+		$(RM) $(RMFLAGS) $(PREFIX)/$(PROFILEDEST)
---
+++ install.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+
+ALACRITTY_SRC="alacritty.yml"
+ALACRITTY_DEST="${HOME}/.config/alacritty/alacritty.yml"
+
+TMUX_SRC="config.tmux"
+TMUX_DEST="${HOME}/.config/tmux/tmux.conf"
+
+BASH_SRC="dist/bashrc.bash"
+BASH_DEST="${HOME}/.bashrc"
+
+BASH_PROFILE_SRC="dist/bash-profile.bash"
+BASH_PROFILE_DEST="${HOME}/.bash_profile"
+
+PROFILE_SRC="dist/profile.sh"
+PROFILE_DEST="${HOME}/.profile"
+
+READLINE_SRC="dist/inputrc.readline"
+READLINE_DEST="${HOME}/.inputrc"
+
+VIM_SRC="dist/config.vim"
+VIM_DEST="${HOME}/.config/vim/config.vim"
+
+create_link() {
+    ln --symbolic --force  \
+       "$(readlink --canonicalize "$2")" "$1"
+}
+
+main() {
+    for file in dist/*; do
+        case "${file}" in
+            "${ALACRITTY_SRC}")
+                create_link "${ALACRITTY_DEST}" "${file}"
+                ;;
+            "${BASH_SRC}")
+                create_link "${BASH_DEST}" "${file}"
+                ;;
+            "${BASH_PROFILE_SRC}")
+                create_link "${BASH_PROFILE_DEST}" "${file}"
+                ;;
+            "${PROFILE_SRC}")
+                create_link "${PROFILE_DEST}" "${file}"
+                ;;
+            "${READLINE_SRC}")
+                create_link "${READLINE_DEST}" "${file}"
+                ;;
+            "${TMUX_SRC}")
+                create_link "${TMUX_DEST}" "${file}"
+                ;;
+            "${VIM_SRC}")
+                create_link "${VIM_DEST}" "${file}"
+                ;;
+        esac
+    done
+}
+
+main "$@"
